2c7c96bb9f242e9613b671c66bb33f779cdf9bad
Cameron Simmons <cssim22@gmail.com>
2021-05-19 20:05:20 -0700

    server is now talking to mongoDB as well

--- server/server.js
+++ server/server.js
@@ -1,25 +1,45 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const apiRouter = require('./routes/api');
 
-// uncomment the below for proxy challenge
+const PORT = 3000;
 
-const leaderList = [
-	{ name: "Anna", id: "a0" },
-	{ name: "Ben", id: "b0" },
-	{ name: "Clara", id: "c0" },
-	{ name: "David", id: "d0" },
-];
 
-app.get("/api/leaders", (req, res) => {
-	return res.status(200).send(leaderList);
+//handle parsing the request body
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// handle requests for static files
+app.use(express.static(path.resolve(__dirname, '../client'))); //THIS IS NOT RIGHT CURRENTLY
+
+
+//define route handler
+app.use('/api',apiRouter);
+
+//
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('Can/t find requested page.'));
+
+
+// GLobal error nahdler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'A middleware express error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
-// statically serve everything in the build folder on the route '/build'
-app.use("/build", express.static(path.join(__dirname, "../build")));
-// serve index.html on the route '/'
-app.get("/", (req, res) => {
-	return res.status(200).sendFile(path.join(__dirname, "../index.html"));
+/**
+ * start server
+ */
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
 });
 
-app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+module.exports = app;
